a0302e4fef6101f9990b7cc8e0ec84324bdfb181
LordWrynn <44822503+LordWrynn@users.noreply.github.com>
2021-02-12 12:36:09 -0700

    Bugfixes from test branch

--- src/main/controllers/Engine.java
+++ src/main/controllers/Engine.java
@@ -23,14 +23,20 @@ public class Engine {
 
     private void createBoard(){
         board = new Piece[19][19];
+<<<<<<< Updated upstream
         for(int y = 0; y < board.length; y++) {
             for(int x = 0; x < board[0].length; x++) {
+=======
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[0].length; x++) {
+>>>>>>> Stashed changes
                 board[y][x] = Piece.EMPTY;
             }
         }
     }
 
     //aiTurn() (get move from AI class. Make move. PassTurn)
+<<<<<<< Updated upstream
     public void aiTurn() {
 
     public boolean validateMove(int y, int x){
@@ -39,6 +45,22 @@ public class Engine {
         } else {
             return true;
         }
+=======
+    public int[] aiTurn() {
+        boolean isValid = false;
+        int[] move;
+        do {
+            move = new int[]{new Random().nextInt(19), new Random().nextInt(19)};
+            isValidMove(move[0], move[1]);
+        } while (!isValid);
+        return move;
+    }
+
+    public boolean isValidMove(int y, int x) {
+        return (y > -1 && y < 19 &&
+                x > -1 && x < 19 &&
+                board[y][x] == Piece.EMPTY);
+>>>>>>> Stashed changes
     }
 
     public boolean makeMove(int y, int x){
@@ -48,11 +70,15 @@ public class Engine {
             } else {
                 board[y][x] = Piece.BLACK;
             }
+<<<<<<< Updated upstream
             return true;
         } else {
             return false;
+=======
+>>>>>>> Stashed changes
         }
     }
+<<<<<<< Updated upstream
     public String passTurn(){
         if(isPlayerOneTurn){
             isPlayerOneTurn = false;
@@ -65,7 +91,13 @@ public class Engine {
             isPlayerOneTurn = true;
             return "It is now " + Controller.p1Name + "'s Turn";
         }
+=======
+
+    public void passTurn() {
+        isPlayerOneTurn = !isPlayerOneTurn();
+>>>>>>> Stashed changes
     }
+
     public boolean checkForCapture(int y, int x) {
         Piece color = board[y][x];
         boolean pTurn;
@@ -81,137 +113,154 @@ public class Engine {
         //Checks horizontally
         if (x > 2) {
             if (board[y][x - 1] == oppColor && board[y][x - 2] == oppColor && board[y][x - 3] == color) {
-                if(pTurn){
+                if (pTurn) {
                     p1Caps++;
                 } else {
                     p2Caps++;
                 }
+                board[y][x-1] = Piece.EMPTY;
+                board[y][x-2] = Piece.EMPTY;
                 return true;
             }
         } else if (x < 14) {
             if (board[y][x + 1] == oppColor && board[y][x + 2] == oppColor && board[y][x + 3] == color) {
-                if(pTurn){
+                if (pTurn) {
                     p1Caps++;
                 } else {
                     p2Caps++;
                 }
+                board[y][x+1] = Piece.EMPTY;
+                board[y][x+2] = Piece.EMPTY;
                 return true;
             }
         }
         //Checks Vertically
         if (y > 2) {
             if (board[y - 1][x] == oppColor && board[y - 2][x] == oppColor && board[y - 3][x] == color) {
-                if(pTurn){
+                if (pTurn) {
                     p1Caps++;
                 } else {
                     p2Caps++;
                 }
+                board[y-1][x] = Piece.EMPTY;
+                board[y-2][x] = Piece.EMPTY;
                 return true;
             }
         } else if (y < 14) {
             if (board[y + 1][x] == oppColor && board[y + 2][x] == oppColor && board[y + 3][x] == color) {
-                if(pTurn){
+                if (pTurn) {
                     p1Caps++;
                 } else {
                     p2Caps++;
                 }
+                board[y+1][x] = Piece.EMPTY;
+                board[y+2][x] = Piece.EMPTY;
                 return true;
             }
         }
-        //Checks Horizontally
-        if(x > 2 && y > 2){
+        //Checks Diagonally
+        if (x > 2 && y > 2) {
             if (board[y - 1][x - 1] == oppColor && board[y - 2][x - 2] == oppColor && board[y - 3][x - 3] == color) {
-                if(pTurn){
+                if (pTurn) {
                     p1Caps++;
                 } else {
                     p2Caps++;
                 }
+                board[y-1][x-1] = Piece.EMPTY;
+                board[y-2][x-2] = Piece.EMPTY;
                 return true;
             }
-        } else if(x < 14 && y < 14){
+        } else if (x < 14 && y < 14) {
             if (board[y + 1][x + 1] == oppColor && board[y + 2][x + 2] == oppColor && board[y + 3][x + 3] == color) {
-                if(pTurn){
+                if (pTurn) {
                     p1Caps++;
                 } else {
                     p2Caps++;
                 }
+                board[y+1][x+1] = Piece.EMPTY;
+                board[y+2][x+2] = Piece.EMPTY;
                 return true;
             }
         }
         return false;
     }
-
-    public boolean checkFor(int y, int x, int num) {
-     //check horizontal
+    private boolean checkFor(int y, int x, int num) {
+        //check horizontal
         Piece color = board[y][x];
 
         int pieces = 1;
-        try{
-            for(int i = x - 1; pieces < 5; i--){
-                if(board[y][i] == color){
+        try {
+            for (int i = x - 1; pieces < 5; i--) {
+                if (board[y][i] == color) {
                     pieces++;
                 } else {
                     throw new Exception();
                 }
             }
-        } catch (Exception e){}
-        try{
-            for(int i = x + 1; pieces < 5; i++){
-                if(board[y][i] == color){
+        } catch (Exception e) {
+        }
+        try {
+            for (int i = x + 1; pieces < 5; i++) {
+                if (board[y][i] == color) {
                     pieces++;
                 } else {
                     throw new Exception();
                 }
             }
-        } catch (Exception e){}
-        if(pieces >= num){
+        } catch (Exception e) {
+        }
+        if (pieces >= num) {
             return true;
         }
         pieces = 1;
-        try{
-            for(int i = y - 1; pieces < 5; i--){
-                if(board[i][x] == color){
+        try {
+            for (int i = y - 1; pieces < 5; i--) {
+                if (board[i][x] == color) {
                     pieces++;
                 } else {
                     throw new Exception();
                 }
             }
-        } catch (Exception e){}
-        try{
-            for(int i = y + 1; pieces < num; i++){
-                if(board[i][x] == color){
+        } catch (Exception e) {
+        }
+        try {
+            for (int i = y + 1; pieces < num; i++) {
+                if (board[i][x] == color) {
                     pieces++;
                 } else {
                     throw new Exception();
                 }
             }
-        } catch (Exception e){}
-        if(pieces >= num){
+        } catch (Exception e) {
+        }
+        if (pieces >= num) {
             return true;
         }
         pieces = 1;
-        try{
+        try {
             int j = y - 1;
-            for(int i = x - 1; pieces < num; i--){
-                    if (board[j][i] == color) {
-                        pieces++;
-                    } else {
-                        throw new Exception();
-                    }
-                    j--;
+            for (int i = x - 1; pieces < num; i--) {
+                if (board[j][i] == color) {
+                    pieces++;
+                } else {
+                    throw new Exception();
+                }
+                j--;
 
             }
-        } catch (Exception e){}
-        try{
+        } catch (Exception e) {
+        }
+        try {
             int j = y + 1;
-            for(int i = x + 1; pieces < num; i++){
-                if(board[j][i] == color){
+            for (int i = x + 1; pieces < num; i++) {
+                if (board[j][i] == color) {
                     pieces++;
                 } else {
                     throw new Exception();
                 }
                 j++;
             }
+<<<<<<< Updated upstream
         } catch (Exception e){}
         if(pieces >= num){
             return true;
@@ -239,5 +288,52 @@ public class Engine {
 
     public Boolean getP2IsAI() {
         return p2IsAI;
+=======
+        } catch (Exception e) {
+        }
+        return pieces >= num;
+    }
+
+    public boolean checkForWin(int y, int x) {
+        if (isPlayerOneTurn && p1Caps >= 5) {
+            return true;
+        } else if (!isPlayerOneTurn && p2Caps >= 5) {
+            return true;
+        } else {
+            return checkFor(y, x, 5);
+        }
+    }
+    public boolean checkForTria(int y, int x) {
+        return checkFor(y, x, 3);
+    }
+    public boolean checkForTesera(int y, int x) {
+        return checkFor(y, x, 4);
+    }
+
+    public int getP1Captures() {
+        return p1Caps;
+    }
+    public int getP2Captures() {
+        return p2Caps;
+    }
+    public void setP1Captures(int p1Captures) {
+        this.p1Caps = (byte) p1Captures;
+    }
+    public void setP2Captures(int p2Captures) {
+        this.p2Caps = (byte) p2Captures;
+    }
+
+    public Boolean isP2Ai() {
+        return isP2Ai;
+    }
+    public Boolean isPlayerOneTurn() {
+        return isPlayerOneTurn;
+    }
+    public Piece[][] getBoard() {
+        return board;
+    }
+    public void setPlayerOneTurn(Boolean playerOneTurn) {
+        isPlayerOneTurn = playerOneTurn;
+>>>>>>> Stashed changes
     }
 }
--- src/main/controllers/GameController.java
+++ src/main/controllers/GameController.java
@@ -59,6 +59,71 @@ public class GameController {
         engine.makeMove(x,y);
         engine.passTurn();
 
+<<<<<<< Updated upstream
+=======
+        boolean isTurnHandled;
+
+        //Attempts to handle turn until handle turn returns true
+        //Uses same turn handling method as player with AI determined [<y>, <x>] values
+        do {
+            yx = engine.aiTurn();
+            isTurnHandled = handleTurn(yx[0], yx[1]);
+        } while (!isTurnHandled);
+
+    }
+
+    private boolean handleTurn(int y, int x) {
+        boolean isTurnHandled = engine.makeMove(y, x);
+        System.out.println("isTurnHandled: " + isTurnHandled);
+        if  (isTurnHandled) {
+//            //Check for win
+//            if (engine.checkFor(y, x, 5)) {
+//
+//            } else {
+//                //Check for tesera
+//                if (engine.checkFor(y, x, 4)) {
+//
+//                }
+//                //If not tesera, check for tria
+//                else if (engine.checkFor(y, x, 3)) {
+//
+//                }
+//                else {
+//
+//                }
+//            }
+            boolean isCaptureFound = engine.checkForCapture(y, x); //Should return coords of captured pieces?
+            System.out.println("Capture: " + isCaptureFound);
+            if (isCaptureFound) {
+                if (engine.isPlayerOneTurn()) {
+                    engine.setP1Captures(getEngine().getP1Captures() + 1);
+                } else {
+                    engine.setP2Captures(getEngine().getP2Captures() + 1);
+                }
+            }
+
+            /*
+            left, down, and the tl<->br diagonal return true for captures
+              \
+              -\
+              | \- -- -- --
+              |  \
+              |   \
+              |
+            */
+
+            conditionStr = (engine.checkForWin(y, x) || //Checks for win by 5 consecutive stones
+                    ((engine.isPlayerOneTurn() ? engine.getP1Captures() : engine.getP2Captures()) >= 5)) ? //Checks for win by 5 captures
+                    ((engine.isPlayerOneTurn() ? playerOneName : playerTwoName) + " wins!") :
+                    (engine.checkForTesera(y, x)) ? //Checks for tesera
+                            ((engine.isPlayerOneTurn() ? playerOneName : playerTwoName) + (" has a tesera")) :
+                            (engine.checkForTria(y, x)) ? //Checks for tria
+                                    ((engine.isPlayerOneTurn() ? playerOneName : playerTwoName) + (" has a tria")) : "";
+            if (conditionStr.toLowerCase().contains("win")) engine.passTurn();
+            engine.passTurn();
+        }
+        return isTurnHandled;
+>>>>>>> Stashed changes
     }
 
     public Engine getEngine() {
